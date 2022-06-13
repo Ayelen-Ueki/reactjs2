f1910438311802b4e914d4c1b608f2947aa7aa70
Ayelen-Ueki <63622584+Ayelen-Ueki@users.noreply.github.com>
2022-06-13 00:51:20 -0300

    Cart Context

--- src/components/context/cartContext.js
+++ src/components/context/cartContext.js
@@ -1,24 +1,28 @@
-import {createContext, useState} from "react";
+import { createContext, useState } from "react";
 
 const CartContext = createContext();
 
-const CartProvider = ({children}) => {
-    const [cartListItems, setCartListItems] = useState ([]);
-    const addProductToCart = (product) => {
-        setCartListItems(cartListItems => [...cartListItems,product])
-    }
-    const data =  {
-        cartListItems,
-        setCartListItems,
-        addProductToCart,
-    }
-    return(
-        <CartContext.Provider value = {data}>
-            {children}
-        </CartContext.Provider>
+const CartProvider = ({ children }) => {
+  const [cartListItems, setCartListItems] = useState([]);
 
-    )
-}
+  const addProductToCart = (product) => {
+    let isInCart = cartListItems.find(
+      (cartItems) => cartItems.id === product.id
+    );
+    if (!isInCart) {
+      setCartListItems((cartListItems) => [...cartListItems, product]);
+    }
+    else{
+        setCartListItems(product.cantidad +1)
+    }
+  };
+  const data = {
+    cartListItems,
+    setCartListItems,
+    addProductToCart,
+  };
+  return <CartContext.Provider value={data}>{children}</CartContext.Provider>;
+};
 
-export {CartContext};
+export { CartContext };
 export default CartProvider;
--- src/components/pages/Carrito.js
+++ src/components/pages/Carrito.js
@@ -1,20 +1,38 @@
-import './ItemList.css';
-import ItemCards from '../itemCards/ItemCards';
-import { useContext } from 'react';
+import "./ItemList.css";
+import ItemCards from "../itemCards/ItemCards";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-    const{cartListItems}=useContext();
+  const { cartListItems } = useContext();
+  {
+    cartListItems.length === 0 && (
+      <>
+        <p>Aún no hay productos agregados</p>
+        <Link to="/Inicio">¿Quieres ver nuestro catálogo?</Link>
+      </>
+    );
+  }
+  {cartListItems.map(({ title, price, image, description, id, stock }) => {
   return (
     <div className="items">
-      {cartListItems.map(({title, price, image, description, id, stock}) => {
+      
         return (
           <div key={id} className="item">
-            <ItemCards title={title} price={price} image={image} description={description} stock={stock} id={id}/>
-          </div>      
+            <ItemCards
+              title={title}
+              price={price}
+              image={image}
+              description={description}
+              stock={stock}
+              id={id}
+            />
+          </div>
         );
-      })}
+
     </div>
   );
+})}
 };
 
 export default Cart;
